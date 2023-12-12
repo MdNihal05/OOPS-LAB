3a65d69f17db95e04bb9303fa8bd069217fbbc2b
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-12-12 10:41:11 +0530

    Create EqualIgnore.java

---
+++ OOPS-LAB/Week-4/EqualIgnore.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class StringEqualityTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first string: ");
+        String str1 = scanner.nextLine();
+
+        System.out.print("Enter the second string: ");
+        String str2 = scanner.nextLine();
+
+        boolean isEqual = str1.equals(str2);
+        boolean isEqualIgnoreCase = str1.equalsIgnoreCase(str2);
+
+        System.out.println("Using equals: " + isEqual);
+        System.out.println("Using equalsIgnoreCase: " + isEqualIgnoreCase);
+    }
+}
