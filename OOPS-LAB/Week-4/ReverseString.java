728e13a5f1b78b5cab21211780bc1800b7f34775
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-12-12 10:54:35 +0530

    Create ReverseString.java

---
+++ OOPS-LAB/Week-4/ReverseString.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class ReverseString{
+	public static void main(String ...args){
+		Scanner in=new Scanner(System.in);
+		String s=in.nextLine();
+		String rev="";
+		for(int i=s.length()-1;i>=0;i--){
+			rev+=s.charAt(i);
+		}
+		System.out.println(rev);
+	}
+}
