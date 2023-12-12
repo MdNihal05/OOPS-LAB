5e624a98722e11af88d0cc88742ed4789e8231c3
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-12-12 10:50:04 +0530

    Create VowelConsonant.java

---
+++ OOPS-LAB/Week-4/VowelConsonant.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine().toLowerCase();
+
+        int vowelCount = 0;
+        int consonantCount = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    System.out.println("Vowel: " + ch);
+                    vowelCount++;
+                } else {
+                    System.out.println("Consonant: " + ch);
+                    consonantCount++;
+                }
+            }
+        }
+
+        System.out.println("Number of vowels: " + vowelCount);
+        System.out.println("Number of consonants: " + consonantCount);
+    }
+}
